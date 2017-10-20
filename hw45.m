42acef470f6ad2c9e1fbe2872484c8cee4dde07a
zsc1123 <32053749+zsc1123@users.noreply.github.com>
2017-10-20 00:39:32 -0700

    3

--- hw45.m
+++ hw45.m
@@ -4,6 +4,10 @@ figure(1)
 scatter3(x1,x2,Y)
 n=1000;
 phi=[ones(n,1) x1 x2];
-theta11=phi\Y;
-Y1=phi*theta11;
+theta1=phi\Y;
+Y1=phi*theta1;
 mse1=mean((Y-Y1).^2);
+[A,B]=meshgrid(x1,x2);
+Y11=theta1(1,1)+theta1(2,1)*A+theta1(3,1)*B;
+figure(2)
+surf(x1,x2,Y11)
